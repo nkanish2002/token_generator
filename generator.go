319a2475c042b495b21706518a54c12b8b2a243f
Anish  Gupta <nkanish2002@gmail.com>
2017-03-22 17:17:12 +0530

    Updating doc

--- generator.go
+++ generator.go
@@ -8,38 +8,38 @@ import (
 
 /*
 Main Generator structure
-	n		Number of generated strings to be cached
-	cache_length	Number of characters in the string
+	N		Number of generated strings to be cached
+	Cache_length	Number of characters in the string
 	cache		Array of generated strings
 */
 type Generator struct {
-	n            int
-	cache_length int
+	N            int
+	Cache_length int
 	cache        []string
 }
 
 /*
 Initializer for the token generator. To initialize with custom settings set:-
-	n		Any positive integer value (default 1000)
-	cache_length	Any positive integer value (default 200)
+	N		Any positive integer value (default 1000)
+	Cache_length	Any positive integer value (default 200)
 */
 func (g *Generator) New() {
-	if g.n == 0 {
-		g.n = 1000
+	if g.N == 0 {
+		g.N = 1000
 	}
-	if g.cache_length == 0 {
-		g.cache_length = 200
+	if g.Cache_length == 0 {
+		g.Cache_length = 200
 	}
-	g.cache = make([]string, g.n)
+	g.cache = make([]string, g.N)
 	rand.Seed(time.Now().Unix())
-	for i := 0; i < g.n; i++ {
-		g.cache[i] = RandomString(g.cache_length)
+	for i := 0; i < g.N; i++ {
+		g.cache[i] = RandomString(g.Cache_length)
 	}
 }
 
 // Token Generator function
 func (g Generator) GetToken(size int) string {
-	i := rand.Intn(g.n)
-	start := rand.Intn(g.cache_length - size - 1)
+	i := rand.Intn(g.N)
+	start := rand.Intn(g.Cache_length - size - 1)
 	return g.cache[i][start : start+size]
 }
